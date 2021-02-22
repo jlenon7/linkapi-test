12415f7a0df007f4e8ba5cf03f87357aac8d2bc7
João Lenon <lenonsec7@gmail.com>
2021-02-22 19:20:32 -0300

    feat: BlingCollection Implementation

--- app/Controllers/Http/Middlewares/PaginationMiddleware.ts
+++ app/Controllers/Http/Middlewares/PaginationMiddleware.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestMiddleware } from '@nestjs/common'
 
 @Injectable()
-export default class PaginationMiddleware implements NestMiddleware {
+export class PaginationMiddleware implements NestMiddleware {
   use(req, res, next) {
     const page = req.query.page ? parseInt(req.query.page) : 0
     const limit = req.query.limit ? parseInt(req.query.limit) : 10
